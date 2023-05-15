b5317dfbc64bef985f405968b847856139705bee
Enrique <enrique.delafuentep@alumnos.upm.es>
2023-05-15 12:50:49 +0200

    Changes by Ignacio Fernandez

--- Implementacion/UPMFit/src/main/java/upm/etsisi/Cliente.java
+++ Implementacion/UPMFit/src/main/java/upm/etsisi/Cliente.java
@@ -7,23 +7,12 @@ public class Cliente extends Usuario {
 
     private int descuentoFijo = 0;
     private int edad;
-    private Date fechaBaja;
-    private float peso;//recuerda pasarlo al enterprise que esto antes era int
+    private float peso;
     private String sexo;
     private String tarjetaCredito;
     private List<Cuota> cuotas;
 
     private List<InscripcionCurso> inscripcionesCurso;
-
-    public Cliente(String nombreUsuario, String nombreCompleto, String correoElectronico, String contrasena, String DNI, int edad,
-                   Date fechaBaja, float peso, String sexo, String tarjetaCredito) {
-        super(nombreUsuario, nombreCompleto, correoElectronico, contrasena, DNI);
-        this.edad = edad;
-        this.fechaBaja = fechaBaja;
-        this.peso = peso;
-        this.sexo = sexo;
-        this.tarjetaCredito = tarjetaCredito;
-    }
 
     public Cliente(String nombreUsuario, String nombreCompleto, String correoElectronico, String contrasena,
                    String DNI, int edad, float peso, String sexo, String tarjetaCredito) {
@@ -44,62 +33,6 @@ public class Cliente extends Usuario {
         datosCliente.put("Numero Tarjeta de credito: ", this.tarjetaCredito);
         datosCliente.put("Decuento aplicado: ", String.valueOf(this.descuentoFijo));
         return datosCliente;
-    }
-
-    public int getDescuentoFijo() {
-        return this.descuentoFijo;
-    }
-
-    public void setDescuentoFijo(int descuentoFijo) {
-        this.descuentoFijo = descuentoFijo;
-    }
-
-    public int getEdad() {
-        return this.edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public Date getFechaBaja() {
-        return this.fechaBaja;
-    }
-
-    public void setFechaBaja(Date fechaBaja) {
-        this.fechaBaja = fechaBaja;
-    }
-
-    public float getPeso() {
-        return this.peso;
-    }
-
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-
-    public String getSexo() {
-        return this.sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getTarjetaCredito() {
-        return this.tarjetaCredito;
-    }
-
-    public void setTarjetaCredito(String tarjetaCredito) {
-        this.tarjetaCredito = tarjetaCredito;
-    }
-
-    public List<Cuota> getCuotas() {
-        return this.cuotas;
-    }
-
-    public void setCuotas(List<Cuota> cuotas) {
-        this.cuotas = cuotas;
     }
 
     public List<InscripcionCurso> getInscripcionesCurso() {
